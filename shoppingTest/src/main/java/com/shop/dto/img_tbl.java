b64ce9cab210ce93f918c50c0e419ee6a6f857de
JJKim <whdwls963@gmail.com>
2023-01-12 17:12:33 +0900

    230112 17-11 jjk img_tbl, product dto 생성 text.java package 삭제

---
+++ shoppingTest/src/main/java/com/shop/dto/img_tbl.java
@@ -0,0 +1,18 @@
+package com.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class img_tbl {
+	private int img_no;
+	private int con_no;
+	private String con_img;
+}
