a2e5cdb146cfb250da55da6baf922ffa8b5b96f7
82109 <82109@BOOK-1C67ONL3M5>
2023-01-12 17:18:54 +0900

    go

---
+++ shoppingTest/src/main/java/com/shop/dto/board.java
@@ -0,0 +1,26 @@
+package com.shop.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class board {
+	private int board_no;
+	private int board_no2;
+	private int con_no;
+	private int mem_no;
+	private Date writedate;
+	private String review;
+	private String partner;
+	private String secret_memo;
+	private int star_rate;
+}
