455c815a0d2528b468040ef7513b125fc451d098
JJKim <whdwls963@gmail.com>
2023-01-17 17:36:04 +0900

    230117 jjk 팔로워페이지 추가

---
+++ shoppingTest/src/main/java/com/shop/controller/JongjinController.java
@@ -0,0 +1,22 @@
+package com.shop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shop.dto.Member_tbl;
+import com.shop.service.Member_tblService;
+
+@Controller
+public class JongjinController {
+	
+	@Autowired
+	Member_tblService mservice;
+	
+	@RequestMapping("/follower")
+    public String follower() {
+        return "follower";
+    }
+	
+}
