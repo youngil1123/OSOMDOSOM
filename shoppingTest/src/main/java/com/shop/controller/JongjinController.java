bee8a646ce92cb5eeccb4f4a33d3f0f14de34929
JJKim <whdwls963@gmail.com>
2023-01-26 17:25:20 +0900

    230126 1725 jjk

--- shoppingTest/src/main/java/com/shop/controller/JongjinController.java
+++ shoppingTest/src/main/java/com/shop/controller/JongjinController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.shop.dto.Friendship;
 import com.shop.dto.Member_tbl;
@@ -27,8 +28,19 @@ public class JongjinController {
 	FriendshipService fservice;
 	
 	@RequestMapping("")
-    public String follower() {
-        return "follower/follower";
+    public ModelAndView follower(HttpServletRequest request) {
+		
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		String myid = session.getId();	//로그인한 아이디
+		
+		int my_no = mservice.findmem_no(myid); //내 회원번호 
+		
+		
+		
+		mv.setViewName("follower/follwer");
+		
+        return mv;
     }
 	
 	@RequestMapping("/addFollower")
