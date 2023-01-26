aa9c0021dcf119d6cc91160c5cdebfd9bf4ebeb7
Dasom Hwang <moa8417@naver.com>
2023-01-26 17:23:27 +0900

    비밀번호확인, 아이디중복체크 했음....

--- shoppingTest/src/main/java/com/shop/mapper/Member_tblMapper.java
+++ shoppingTest/src/main/java/com/shop/mapper/Member_tblMapper.java
@@ -13,6 +13,6 @@ public interface Member_tblMapper extends MyMapper<String,Member_tbl>{
 	public Integer findmem_no(String mem_id);
 	public String findid(String mem_name, String mem_email);
 	public String findpwd(String mem_id, String mem_email);
-	public int idCheck(String mem_id);
+	public Integer idCheck(String mem_id);
 
 }
