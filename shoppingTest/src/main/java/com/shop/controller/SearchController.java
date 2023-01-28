6bbc024eb7e7be48c8ba2a677ab07d3d2d7cba1e
jeongeunShim <sje11900@naver.com>
2023-01-28 10:35:04 +0900

    dd booklist search 되는겅임?~~

--- shoppingTest/src/main/java/com/shop/controller/SearchController.java
+++ shoppingTest/src/main/java/com/shop/controller/SearchController.java
@@ -18,48 +18,54 @@ import com.shop.service.TheaterService;
 
 @Controller
 public class SearchController {
-	
+
 	@Autowired
 	private BookMapper bookmapper;
-	
+
 	@Autowired
 	BookService bookService;
-	
+
 	@Autowired
 	ContentsService contentsService;
-	
+
 	@Autowired
 	MovieService movieService;
-	
+
 	@Autowired
 	TheaterService theaterService;
-	
-	
-	
-	
-	
+
 	@RequestMapping("/boardwrite")
-    public String boardwrite(Model model) {
-		
+	public String boardwrite(Model model) {
+
 		model.addAttribute("top", "boardwrite");
-        return "/board/boardwrite";
+		return "/board/boardwrite";
 	}
-	
-	//새 글 작성  페이지
-		@RequestMapping(value="/newboardwrite")
-	    public String newboardwrite(Model model) throws Exception {
-			
-			model.addAttribute("top", "newboardwrite");
-	        return "/board/newboardwrite";
+
+	//새 글 작성 페이지
+	@RequestMapping(value = "/newboardwrite")
+	public String newboardwrite(Model model,String keyword,String searchType) throws Exception {
+
+		model.addAttribute("top", "newboardwrite");
+		System.out.println("새글페이지"+keyword +" "+searchType);
+		if(searchType == null) return "/board/newboardwrite";
+		if(searchType.equals("book")) {
+			System.out.println("검색어 입력시?");
+			List<BookInfo> objs = new ArrayList<BookInfo>();
+			objs = bookService.searchbooklist(keyword);
+			System.out.println(objs);
+			model.addAttribute("objs", objs);
 		}
-		
-		
-		@RequestMapping(value="/searchbooklist")
-		@ResponseBody
-	    public List<BookInfo> searchbooklist(String keyword, Model model) throws Exception {
-			
-			List<BookInfo> objs = bookmapper.searchbooklist(keyword);
-			
-	        return objs;
-	    }
+
+		return "/board/newboardwrite";
+	}
+
+	@RequestMapping(value = "/searchbooklist")
+	@ResponseBody
+	public List<BookInfo> searchbooklist(String keyword, Model model) throws Exception {
+		System.out.println("여기 넘어옴?");
+		List<BookInfo> objs = new ArrayList<BookInfo>();
+		objs = bookService.searchbooklist(keyword);
+		System.out.println(objs);
+		return objs;
+	}
 }
