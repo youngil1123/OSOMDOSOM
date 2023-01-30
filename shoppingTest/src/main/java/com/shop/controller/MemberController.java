0d91b0ff5831494104f668fe09f75c89e699da18
jeongeunShim <sje11900@naver.com>
2023-01-30 19:01:04 +0900

    230130 passwd 암호화(회원가입시) 로그인시 복호화해서 로그인되게 해놈~

--- shoppingTest/src/main/java/com/shop/controller/MemberController.java
+++ shoppingTest/src/main/java/com/shop/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shop.dto.Member_tbl;
+import com.shop.frame.CryptoUtil;
 import com.shop.service.Member_tblService;
 
 @Controller
@@ -30,12 +31,18 @@ public class MemberController {
 	
 	//회원가입 데이터 연동
 	@PostMapping("/new")
-	public String create(Member_tbl member_tbl) {
+	public String create(Member_tbl member_tbl) throws Exception{
 		//post로 넘어온 input 데이터를 매게변수로 입력한 MemberForm에 있는 id에 자동으로 setName 이 됨
+		//비밀번호 암호화
+		String plainpwd=member_tbl.getMem_pwd();
+		String key = "osomdosompasswd0077";
+		String encryptpwd = CryptoUtil.encryptAES256(plainpwd, key);
+		System.out.println("AES 256 방식 암호화 : " + encryptpwd);
+		
 	Member_tbl member= new Member_tbl(); 
 		member.setMem_name(member_tbl.getMem_name());
 		member.setMem_id(member_tbl.getMem_id());
-		member.setMem_pwd(member_tbl.getMem_pwd());
+		member.setMem_pwd(encryptpwd);
 		member.setMem_email(member_tbl.getMem_email());
 		member.setMbti(member_tbl.getMbti());
 		member.setNickname(member_tbl.getNickname());
